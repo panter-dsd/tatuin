0342b65a7659e406e301bd0adf89a232e95a46d0
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-07-01 17:21:02 +0300

    Remove unused fields from Issue.

--- src/gitlab/structs.rs
+++ src/gitlab/structs.rs
@@ -91,67 +91,10 @@ pub struct Milestone {
 pub struct Issue {
     pub state: Option<String>,
     pub description: Option<String>,
-    pub author: Option<Author>,
-    pub milestone: Option<Milestone>,
-    pub project_id: Option<i64>,
-    pub assignees: Option<Vec<Assignee>>,
-    pub assignee: Option<Assignee>,
-    pub type_field: Option<String>,
-    pub updated_at: Option<String>,
-    pub closed_at: Option<String>,
-    pub closed_by: Option<String>,
     pub id: i64,
     pub title: Option<String>,
     pub created_at: String,
-    pub moved_to_id: Option<String>,
     pub iid: i64,
-    pub labels: Option<Vec<String>>,
-    pub upvotes: Option<i64>,
-    pub downvotes: Option<i64>,
-    pub merge_requests_count: Option<i64>,
-    pub user_notes_count: Option<i64>,
     pub due_date: Option<String>,
-    pub imported: Option<bool>,
-    pub imported_from: Option<String>,
-    pub web_url: Option<String>,
-    pub references: Option<References>,
-    pub time_stats: Option<TimeStats>,
-    pub has_tasks: Option<bool>,
-    pub task_status: Option<String>,
-    pub confidential: Option<bool>,
-    pub discussion_locked: Option<bool>,
     pub issue_type: String,
-    pub severity: Option<String>,
-    pub links: Option<Links>,
-    pub task_completion_status: Option<TaskCompletionStatus>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct References {
-    pub short: Option<String>,
-    pub relative: Option<String>,
-    pub full: Option<String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct TimeStats {
-    pub time_estimate: Option<i64>,
-    pub total_time_spent: Option<i64>,
-    pub human_time_estimate: Option<String>,
-    pub human_total_time_spent: Option<String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Links {
-    pub self_field: Option<String>,
-    pub notes: Option<String>,
-    pub award_emoji: Option<String>,
-    pub project: Option<String>,
-    pub closed_as_duplicate_of: Option<String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct TaskCompletionStatus {
-    pub count: Option<i64>,
-    pub completed_count: Option<i64>,
 }
