5a54f713b830af476fa150998e4d2dd1c61fe57f
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-07-13 12:21:33 +0300

    Refactoring.

--- src/ui/dialogs.rs
+++ src/ui/dialogs.rs
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
 pub mod dialog;
-pub mod key_bindings_help_dialog;
-pub mod list_dialog;
-pub mod states_dialog;
-pub mod text_input_dialog;
+pub mod key_bindings_help;
+pub mod list;
+pub mod states;
+pub mod text_input;
 
 pub use dialog::DialogTrait;
-pub use key_bindings_help_dialog::Dialog as KeyBindingsHelpDialog;
-pub use list_dialog::Dialog as ListDialog;
-pub use states_dialog::Dialog as StatesDialog;
-pub use text_input_dialog::Dialog as TextInputDialog;
+pub use key_bindings_help::Dialog as KeyBindingsHelpDialog;
+pub use list::Dialog as ListDialog;
+pub use states::Dialog as StatesDialog;
+pub use text_input::Dialog as TextInputDialog;
