a7e5432d7fdbcbfb33046a00b0b53b715b7b6309
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-10-12 21:00:27 +0300

    #266: add migration and replacet xdg using to dirs everywhere.

--- src/migration/xdg_to_dirs.rs
+++ src/migration/xdg_to_dirs.rs
@@ -1,23 +1,96 @@
-use std::io::ErrorKind;
+use std::path::Path;
+
+use tatuin_core::folders;
 
 pub fn migrate_config(app_name: &str, file_name: &str) {
+    let config_dir = folders::config_folder(app_name);
+    let new_config_file = config_dir.join(file_name);
+    if std::fs::exists(&new_config_file).is_ok_and(|r| r) {
+        // do nothing because of new configuration exists
+        return;
+    }
+
     let xdg_dirs = xdg::BaseDirectories::with_prefix(app_name);
     if let Some(config_file) = xdg_dirs.get_config_file(file_name)
         && std::fs::exists(&config_file).is_ok_and(|r| r)
     {
-        let config_dir = dirs::config_dir().expect("Can't detect config dir").join(app_name);
-        if let Err(e) = std::fs::create_dir(&config_dir)
-            && e.kind() != ErrorKind::AlreadyExists
-        {
-            panic!("Create a config dir {config_dir:?}: {e}");
-        }
-
-        let new_config_file = config_dir.join(file_name);
+        println!("Copy the config file {config_file:?} to the new destination {new_config_file:?}");
         if let Err(e) = std::fs::copy(&config_file, &new_config_file) {
             panic!("Copy config from {config_file:?} to {new_config_file:?}: {e}");
         }
+
+        println!("Remove the old config file {config_file:?}");
         if let Err(e) = std::fs::remove_file(&config_file) {
             panic!("Remove the old config file {config_file:?}: {e}");
         }
+
+        migrate_themes(app_name);
+        migrate_cache(app_name);
     }
+}
+
+fn migrate_themes(app_name: &str) {
+    println!("Migrate theme files to the new location");
+
+    let config_folder = xdg::BaseDirectories::with_prefix(app_name)
+        .get_config_home()
+        .expect("Can't get config folder");
+    if let Ok(read_dir) = std::fs::read_dir(&config_folder) {
+        for entry in read_dir {
+            if let Ok(e) = entry
+                && let p = e.path()
+                && p.is_file()
+                && p.extension().unwrap_or_default() == "theme"
+            {
+                let new_name = folders::config_folder(app_name).join(p.file_name().expect("Can't get a file name"));
+
+                println!("Copy the theme file {p:?} to the new destination {new_name:?}");
+                if let Err(e) = std::fs::copy(&p, &new_name) {
+                    panic!("Copy theme file from {p:?} to {new_name:?}: {e}");
+                }
+
+                println!("Remove the old theme file {p:?}");
+                if let Err(e) = std::fs::remove_file(&p) {
+                    panic!("Remove the theme file {p:?} from old location: {e}");
+                }
+            }
+        }
+    }
+}
+
+fn migrate_cache(app_name: &str) {
+    println!("Migrate provider's cache");
+
+    let old_cache = xdg::BaseDirectories::with_prefix(app_name)
+        .get_cache_home()
+        .expect("Can't get old cache path");
+    if !std::fs::exists(&old_cache).is_ok_and(|r| r) {
+        return;
+    }
+
+    let new_cache = folders::cache_folder(app_name);
+
+    println!("Copy the folder {old_cache:?} to the new destination {new_cache:?}");
+    if let Err(e) = copy_recursively(&old_cache, &new_cache) {
+        panic!("Copy cache folder {old_cache:?} to {new_cache:?}: {e}");
+    }
+
+    println!("Remove the old cache {old_cache:?}");
+    if let Err(e) = std::fs::remove_dir_all(&old_cache) {
+        panic!("Remove old cache folder {old_cache:?}: {e}");
+    }
+}
+
+fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> std::io::Result<()> {
+    std::fs::create_dir_all(&destination)?;
+    for entry in std::fs::read_dir(source)? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            std::fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
--- tatuin-core/src/folders.rs
+++ tatuin-core/src/folders.rs
@@ -3,13 +3,22 @@
 use std::{io::ErrorKind, path::PathBuf};
 
 pub fn cache_folder(app_name: &str) -> PathBuf {
-    let xdg_dirs = xdg::BaseDirectories::with_prefix(app_name);
-    xdg_dirs.create_cache_directory("").expect("cannot create cache folder")
+    let p = dirs::cache_dir().expect("Can't detect cache folder").join(app_name);
+    create_dir_paniced(&p);
+    p
 }
 
 pub fn state_folder(app_name: &str) -> PathBuf {
-    let xdg_dirs = xdg::BaseDirectories::with_prefix(app_name);
-    xdg_dirs.create_state_directory("").expect("cannot create state folder")
+    let p = if cfg!(target_os = "macos") {
+        dirs::home_dir()
+            .expect("Can't detect home folder")
+            .join("Library/Logs")
+            .join(app_name)
+    } else {
+        dirs::state_dir().expect("Can't detect state folder").join(app_name)
+    };
+    create_dir_paniced(&p);
+    p
 }
 
 pub fn log_folder(app_name: &str) -> PathBuf {
@@ -17,21 +26,33 @@ pub fn log_folder(app_name: &str) -> PathBuf {
 }
 
 pub fn config_folder(app_name: &str) -> PathBuf {
-    let xdg_dirs = xdg::BaseDirectories::with_prefix(app_name);
-    xdg_dirs.get_config_home().expect("cannot create config folder")
+    let p = dirs::config_dir().expect("Can't detect config dir").join(app_name);
+    create_dir_paniced(&p);
+    p
 }
 
-pub fn provider_cache_folder(app_name: &str, provider_name: &str) -> Result<PathBuf, std::io::Error> {
+pub fn provider_cache_folder(app_name: &str, provider_name: &str) -> std::io::Result<PathBuf> {
     let path = cache_folder(app_name).join(provider_name);
-    if let Err(e) = std::fs::create_dir(&path)
-        && e.kind() != ErrorKind::AlreadyExists
-    {
-        return Err(e);
-    }
-
+    create_dir(&path)?;
     Ok(path)
 }
 
 pub fn temp_folder() -> PathBuf {
     std::env::temp_dir()
+}
+
+pub fn create_dir_paniced(p: &PathBuf) {
+    if let Err(e) = create_dir(p) {
+        panic!("Can't create the path {p:?}: {e}");
+    }
+}
+
+pub fn create_dir(p: &PathBuf) -> Result<(), std::io::Error> {
+    if let Err(e) = std::fs::create_dir(p)
+        && e.kind() != ErrorKind::AlreadyExists
+    {
+        return Err(e);
+    }
+
+    Ok(())
 }
