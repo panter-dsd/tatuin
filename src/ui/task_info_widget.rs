6b4eaca07df43d2949531cf7fc96f44269ed71c2
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-11-19 06:38:16 +0300

    #324: don't show empty description in the TaskInfo panel.

--- src/ui/task_info_widget.rs
+++ src/ui/task_info_widget.rs
@@ -129,7 +129,9 @@ impl TaskInfoWidget {
                 widget: Box::new(Text::new(t.priority().to_string().as_str())),
             });
 
-            if let Some(d) = t.description() {
+            if let Some(d) = t.description()
+                && !d.trim().is_empty()
+            {
                 entries.push(Entry {
                     title: "Description".to_string(),
                     widget: Box::new(MarkdownView::new(
