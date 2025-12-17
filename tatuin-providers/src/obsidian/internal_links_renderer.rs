e01d49edbdae36206bd7cd2b62281c1a41c55de5
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-12-17 06:52:09 +0300

    Fix typo.

--- tatuin-providers/src/obsidian/internal_links_renderer.rs
+++ tatuin-providers/src/obsidian/internal_links_renderer.rs
@@ -40,7 +40,7 @@ impl InternalLinksRenderer {
 
     pub fn set_vault_path(&mut self, p: &Path) {
         if self.vault_path.is_none() {
-            self.display = fix_refular_links(self.display.as_str(), p);
+            self.display = fix_regular_links(self.display.as_str(), p);
             self.display = fix_wiki_links(self.display.as_str(), p);
             self.vault_path = Some(p.to_path_buf());
         }
@@ -78,7 +78,7 @@ fn fix_wiki_links(text: &str, vault_path: &Path) -> String {
     result
 }
 
-fn fix_refular_links(text: &str, vault_path: &Path) -> String {
+fn fix_regular_links(text: &str, vault_path: &Path) -> String {
     let mut result = text.to_string();
 
     for l in markdown::find_regular_links(text).iter().rev() {
