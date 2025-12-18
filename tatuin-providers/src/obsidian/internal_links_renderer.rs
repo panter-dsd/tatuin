16979317384da4373066a427b99ddd0f62d6586b
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-12-18 07:15:02 +0300

    #339: fix url detection. Add test.

--- tatuin-providers/src/obsidian/internal_links_renderer.rs
+++ tatuin-providers/src/obsidian/internal_links_renderer.rs
@@ -11,7 +11,7 @@ use tatuin_core::RichStringTrait;
 
 use crate::obsidian::{fs, markdown, md_file::TAG_RE};
 
-static URL_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\w+:\/\/[^\s]+").unwrap());
+static URL_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\w+://[^\s$)]+").unwrap());
 
 #[derive(Debug, Clone, Default)]
 pub struct InternalLinksRenderer {
@@ -106,18 +106,30 @@ fn fix_raw_links(text: &str) -> String {
     let mut last_end: usize = 0;
 
     for m in URL_RE.find_iter(text) {
-        // check correctness of the url because the regexp is very simple
-        if url::Url::parse(m.as_str()).is_err() {
+        let start = m.start();
+        if start > 3 /*^[](http://) case*/ && text.get(start-2..start).is_some_and(|s| s == "](") {
+            // detect []() link
             continue;
         }
+        let mut s = m.as_str();
+        let mut end = m.end();
+        // check correctness of the url because the regexp is very simple
+        if url::Url::parse(s).is_err() {
+            if url::Url::parse(&s[..s.len() - 1]).is_ok() {
+                end -= 1;
+                s = &s[..s.len() - 1];
+            } else {
+                continue;
+            }
+        }
 
-        result.push_str(&text[last_end..m.start()]);
+        result.push_str(&text[last_end..start]);
         result.push('[');
-        result.push_str(m.as_str());
+        result.push_str(s);
         result.push_str("](");
-        result.push_str(m.as_str());
+        result.push_str(s);
         result.push(')');
-        last_end = m.end();
+        last_end = end;
     }
     result.push_str(&text[last_end..]);
     result
@@ -139,5 +151,35 @@ impl RichStringTrait for InternalLinksRenderer {
 
     fn display(&self) -> String {
         self.display.clone()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::fix_raw_links;
+
+    #[test]
+    fn fix_raw_links_test() {
+        const FIXTURE:&str = "
+obsidian://open?vault=personal&file=%D0%92%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%BA%D1%80%D0%B0%D1%81%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D0%BE%D0%BB%D0%BE%D1%81
+[link](obsidian://open?vault=personal&file=%D0%92%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%BA%D1%80%D0%B0%D1%81%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D0%BE%D0%BB%D0%BE%D1%81)
+Some http://some another http://hello/: and one more (http://yeeeee)
+- [ ] Some task http://yandex.ru/some/uri 📅 2025-09-18
+    First line https://login:password@host:1243/path/inside/file.txt: here
+    Second line [link](http://ya.ru)
+    Third line
+";
+        const RESULT:&str = "
+[obsidian://open?vault=personal&file=%D0%92%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%BA%D1%80%D0%B0%D1%81%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D0%BE%D0%BB%D0%BE%D1%81](obsidian://open?vault=personal&file=%D0%92%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%BA%D1%80%D0%B0%D1%81%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D0%BE%D0%BB%D0%BE%D1%81)
+[link](obsidian://open?vault=personal&file=%D0%92%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%BA%D1%80%D0%B0%D1%81%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D0%BE%D0%BB%D0%BE%D1%81)
+Some [http://some](http://some) another [http://hello/:](http://hello/:) and one more ([http://yeeeee](http://yeeeee))
+- [ ] Some task [http://yandex.ru/some/uri](http://yandex.ru/some/uri) 📅 2025-09-18
+    First line [https://login:password@host:1243/path/inside/file.txt:](https://login:password@host:1243/path/inside/file.txt:) here
+    Second line [link](http://ya.ru)
+    Third line
+";
+
+        let t = fix_raw_links(FIXTURE);
+        assert_eq!(RESULT, t);
     }
 }
